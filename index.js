0a66e4171f876b9f5dfcc66d4254345eb93f6912
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-04 02:02:03 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//function declaration 
+function declaration(number) {
+    return number * number;
+}
+var expression = function (number) {
+    return number * number;
+};
+console.log(declaration(5));
+console.log(expression(5));
---
+++ index.ts
@@ -0,0 +1,13 @@
+//function declaration 
+function declaration(number:number):number{
+    return number*number;
+}
+
+
+const expression = function(number:number):number{
+    return number*number
+};
+
+
+console.log(declaration(5));
+console.log(expression(5))
